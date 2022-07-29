8a7113c3197f21a9442a2801124391ee582f02ca
sbutler-gh <sam@ysli.be>
2022-07-29 17:10:19 +0200

    Changed links to hyperlinks in data, and removed old site version for cloudflare compatability

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -78,17 +78,17 @@
   <p>⚡ For the same amount of batteries that go in a single electric truck sold for $70,000, we could produce +250 electric bikes sold for $1,000,000.</p>
   <p>Sources of data:</p>
   <ul>
-    <li>The average cost of owning a car is <a href="https://www.nerdwallet.com/article/loans/auto-loans/total-cost-owning-car">$9,666 per year</a>, including car payments, insurance, gas, gas taxes, parking, maintenance, and other costs. (https://www.nerdwallet.com/article/loans/auto-loans/total-cost-owning-car)
+    <li>The average cost of owning a car is <a href="https://www.nerdwallet.com/article/loans/auto-loans/total-cost-owning-car">$9,666 per year</a>, including car payments, insurance, gas, gas taxes, parking, maintenance, and other costs.
       <ul>
         <li>Across 130 million commuters in the USA, that costs us $1,300,000,000,000 per year.</li>
       </ul>
     </li>
-    <li>Across the US, state and local governments pay $617 per capita on highways and roads every year.
+    <li>Across the US, state and local governments pay <a href="https://www.urban.org/policy-centers/cross-center-initiatives/state-and-local-finance-initiative/state-and-local-backgrounders/highway-and-road-expenditures">$617 per capita</a> on highways and roads every year.
       <ul>
         <li>With 329 million people in the United States, that costs us $203 billion every year.</li>
       </ul>
     </li>
-    <li>The total we pay for parking in the United States amounts to $2,300 per vehicle. (https://usa.streetsblog.org/2017/07/26/if-americans-paid-for-the-parking-we-consume-wed-drive-500-billion-fewer-miles-each-year/)
+    <li>The total we pay for parking in the United States amounts to <a href="https://usa.streetsblog.org/2017/07/26/if-americans-paid-for-the-parking-we-consume-wed-drive-500-billion-fewer-miles-each-year/">$2,300 per vehicle</a>.
       <ul>
         <li>With 230 million vehicle in the US, we pay approximately $529 billion every year on parking spaces.</li>
       </ul>
@@ -108,7 +108,7 @@
         <li>Across the USA, new car parking costs X per year, and existing parking loses X in real estate per year.</li>
       </ul>
     </li> -->
-    <li>The F-150 Lightning battery weights 1,800 pounds. (https://fordauthority.com/2021/05/2022-ford-f-150-lightning-battery-reportedly-weighs-1800-pounds/).  The average e-bike battery weighs 7 pounds. (https://www.sixthreezero.com/blogs/bike-advice/how-much-does-an-electric-battery-weigh-or-how-heavy-are-e-bike-batteries)
+    <li>The F-150 Lightning battery weights <a href="https://fordauthority.com/2021/05/2022-ford-f-150-lightning-battery-reportedly-weighs-1800-pounds/">1,800 pounds</a>.  The average e-bike battery weighs <a href="https://www.sixthreezero.com/blogs/bike-advice/how-much-does-an-electric-battery-weigh-or-how-heavy-are-e-bike-batteries">7 pounds</a>.
       <!-- <ul>
         <li>Across the USA, new car parking costs X per year, and existing parking loses X in real estate per year.</li>
       </ul> -->
@@ -127,29 +127,24 @@
   <Chart data={cepp_data} type="line" lineOptions={lineOptions} colors={['green', 'red']}/>
   <p>Sources of data:</p>
   <ul>
-    <li>The $150B clean electricity performance plan would inject $907 billion into the U.S. economy and raise $154 billion in tax revenue for federal, state, and local governments.(https://www.nrdc.org/media/2021/210909)
+    <li>The $150B clean electricity performance plan would inject $907 billion into the U.S. economy and raise $154 billion in tax revenue for federal, state, and local governments. <a href="https://www.nrdc.org/media/2021/210909">Source</a>.
       <!-- <ul>
         <li>For X drivers in the USA, that is X per year.</li>
       </ul> -->
     </li>
-    <li>$150 billion in CEPP investments could reduce Americans’ electricity bills by 5%, compared to the same power sector transformation without federal grants. (https://www.evergreenaction.com/blog/the-clean-electricity-performance-program-is-key-to-delivering-on-climate)
+    <li>$150 billion in CEPP investments could <a href="https://www.evergreenaction.com/blog/the-clean-electricity-performance-program-is-key-to-delivering-on-climate">reduce Americans’ electricity bills by 5%</a>, compared to the same power sector transformation without federal grants.
       <!-- <ul>
         <li>For X drivers in the USA, that is X per year.</li>
+      </ul> -->
+    </li>
+    <li>In 2022, the average price of electricity in the US is expected to be <a href="https://www.eia.gov/todayinenergy/detail.php?id=51438">14.26 cents/kwh</a>, and the US is expected to use <a href="https://www.eia.gov/energyexplained/electricity/use-of-electricity.php#:~:text=Electricity%20consumption%20in%20the%20United,important%20to%20the%20U.S.%20economy">3.9 trillion kilowatt hours</a>.
+      <!-- <ul>
+        <li>Across the USA, that is X per year.</li>
       </ul> -->
     </li>
     <li>With Americans expected to pay $556 billion in electricity costs in 2022, a 5% reduction means $27,807,000,000 in savings per year.  That is $278 billion in savings over a ten-year period, for a policy that costs $150 billion.
       <!-- <ul>
         <li>For X drivers in the USA, that is X per year.</li>
-      </ul> -->
-    </li>
-    <li>In 2022, the average price of electricity in the US is expected to be 14.26 cents/kwh. (https://www.eia.gov/todayinenergy/detail.php?id=51438)
-      <!-- <ul>
-        <li>Across the USA, that is X per year.</li>
-      </ul> -->
-    </li>
-    <li style="word-wrap: break-word">In 2022, the US is expected to use 3.98 trillion kilowatt hours of electricity. (https://www.eia.gov/energyexplained/electricity/use-of-electricity.php#:~:text=Electricity%20consumption%20in%20the%20United,important%20to%20the%20U.S.%20economy.)
-      <!-- <ul>
-        <li>Across the USA, new car parking costs X per year, and existing parking loses X in real estate per year.</li>
       </ul> -->
     </li>
   </ul>
